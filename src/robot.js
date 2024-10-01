d104f8ca9744017c2713ea9f6c7b8bc88e213600
0xdave1 <josephziondavid@gmail.com>
2024-10-01 16:42:43 +0100

    first commit

---
+++ src/robot.js
@@ -0,0 +1,27 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Joseph David',
+    username: '0xdave',
+    email: 'josephtaiwodavid@gmai.com'
+  },
+  {
+    id: 2,
+    name: 'Jeremiah Abiola',
+    username: 'JerrySax',
+    email: 'jerrysax@gmai.com'
+  },
+  {
+    id: 3,
+    name: 'Stephen Onsachi',
+    username: 'stephenroyal',
+    email: 'stephenroya8984@gmai.com'
+  },
+  {
+    id: 4,
+    name: 'Hanson Faith',
+    username: 'Philos',
+    email: 'Hansonfaith0@gmai.com'
+  },
+  
+]
